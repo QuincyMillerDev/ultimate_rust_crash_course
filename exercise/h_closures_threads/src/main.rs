f862c819fa4843fe927f3437c60ce549d97e9fcc
QuincyMillerDev <quibbles007@gmail.com>
2024-10-15 13:55:32 -0400

    done

--- exercise/h_closures_threads/src/main.rs
+++ exercise/h_closures_threads/src/main.rs
@@ -17,7 +17,9 @@ fn expensive_sum(v: Vec<i32>) -> i32 {
     // will need to dereference it each time you use it in the expression like this: `*x`
     v.iter()
         // .filter() goes here
+        .filter(|&x| x % 2 == 0)
         // .map() goes here
+        .map(|&x| x * x)
         .sum()
 }
 
@@ -32,7 +34,7 @@ fn main() {
     // join handle in a variable called `handle`. Once you've done this you should be able to run
     // the code and see the Child thread output in the middle of the main thread's letters
     //
-    //let handle = ...
+    let handle = thread::spawn(move || expensive_sum(my_vector));
 
     // While the child thread is running, the main thread will also do some work
     for letter in vec!["a", "b", "c", "d", "e", "f"] {
@@ -45,8 +47,8 @@ fn main() {
     // to exit with a `Result<i32, Err>`.  Get the i32 out of the result and store it in a `sum`
     // variable.  Uncomment the println.  If you did 1a and 1b correctly, the sum should be 20.
     //
-    //let sum =
-    //println!("The child thread's expensive sum is {}", sum);
+    let sum = handle.join().unwrap();
+    println!("The child thread's expensive sum is {}", sum);
 
     // Time for some fun with threads and channels!  Though there is a primitive type of channel
     // in the std::sync::mpsc module, I recommend always using channels from the crossbeam crate,
@@ -56,16 +58,15 @@ fn main() {
     // flow of execution works.  Once you understand it, alter the values passed to the `pause_ms()`
     // calls so that both the "Thread B" outputs occur before the "Thread A" outputs.
 
-    /*
     let (tx, rx) = channel::unbounded();
     // Cloning a channel makes another variable connected to that end of the channel so that you can
     // send it to another thread.
     let tx2 = tx.clone();
 
     let handle_a = thread::spawn(move || {
-        pause_ms(0);
+        pause_ms(400);
         tx2.send("Thread A: 1").unwrap();
-        pause_ms(200);
+        pause_ms(800);
         tx2.send("Thread A: 2").unwrap();
     });
 
@@ -89,12 +90,44 @@ fn main() {
     // Join the child threads for good hygiene.
     handle_a.join().unwrap();
     handle_b.join().unwrap();
-    */
+
 
     // Challenge: Make two child threads and give them each a receiving end to a channel.  From the
     // main thread loop through several values and print each out and then send it to the channel.
     // On the child threads print out the values you receive. Close the sending side in the main
     // thread by calling `drop(tx)` (assuming you named your sender channel variable `tx`).  Join
     // the child threads.
+
+    // Create separate channels for each child thread
+    let (tx_a, rx_a) = channel::unbounded();
+    let (tx_b, rx_b) = channel::unbounded();
+
+    let handle_ca = thread::spawn(move || {
+        for msg in rx_a {
+            println!("Child thread A: Received {}", msg);
+        }
+    });
+
+    let handle_cb = thread::spawn(move || {
+        for msg in rx_b {
+            println!("Child thread B: Received {}", msg);
+        }
+    });
+
+    // From the main thread loop through several values and print each out and then send it to the channel
+    for value in vec!["one", "two", "three", "four", "five"] {
+        println!("Main thread: Sending {}", value);
+        tx_a.send(value).unwrap();
+        tx_b.send(value).unwrap();
+    }
+
+    // Close the sending side in the main thread
+    drop(tx_a);
+    drop(tx_b);
+
+    // Wait for child threads to finish
+    handle_ca.join().unwrap();
+    handle_cb.join().unwrap();
+
     println!("Main thread: Exiting.")
 }
--- exercise/z_final_project/src/main.rs
+++ exercise/z_final_project/src/main.rs
@@ -25,64 +25,135 @@
 //
 //     let positive_number: u32 = some_string.parse().expect("Failed to parse a number");
 
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+
 fn main() {
-    // 1. First, you need to implement some basic command-line argument handling
-    // so you can make your program do different things.  Here's a little bit
-    // to get you started doing manual parsing.
-    //
-    // Challenge: If you're feeling really ambitious, you could delete this code
-    // and use the "clap" library instead: https://docs.rs/clap/2.32.0/clap/
-    let mut args: Vec<String> = std::env::args().skip(1).collect();
-    if args.is_empty() {
+    let app = App::new("final_project")
+        .version("1.0")
+        .about("A simple app that processes images")
+        .subcommand(SubCommand::with_name("blur")
+            .about("Blur the image")
+            .arg(Arg::with_name("infile")
+                .help("input file to blur")
+                .required(true)
+                .index(1))
+            .arg(Arg::with_name("outfile")
+                .help("file to save the blur image")
+                .required(true)
+                .index(2))
+            .arg(Arg::with_name("amount")
+                .help("amount to blur")
+                .required(true)
+                .index(3)))
+        .subcommand(SubCommand::with_name("brighten")
+            .about("Brighten the image")
+            .arg(Arg::with_name("infile")
+                .help("input file to brighten")
+                .required(true)
+                .index(1))
+            .arg(Arg::with_name("outfile")
+                .help("file to save the brighten image")
+                .required(true)
+                .index(2))
+            .arg(Arg::with_name("amount")
+                .help("amount to brighten")
+                .required(true)
+                .index(3)))
+        .subcommand(SubCommand::with_name("crop")
+            .about("Crop the image")
+            .arg(Arg::with_name("infile")
+                .help("input file to crop")
+                .required(true)
+                .index(1))
+            .arg(Arg::with_name("outfile")
+                .help("file to save the crop image")
+                .required(true)
+                .index(2))
+            .arg(Arg::with_name("x")
+                .help("x coordinate")
+                .required(true)
+                .index(3))
+            .arg(Arg::with_name("y")
+                .help("y coordinate")
+                .required(true)
+                .index(4))
+            .arg(Arg::with_name("width")
+                .help("width")
+                .required(true)
+                .index(5))
+            .arg(Arg::with_name("height")
+                .help("height")
+                .required(true)
+                .index(6)))
+        .subcommand(SubCommand::with_name("rotate")
+            .about("Rotate the image")
+            .arg(Arg::with_name("infile")
+                .help("input file to rotate")
+                .required(true)
+                .index(1))
+            .arg(Arg::with_name("outfile")
+                .help("file to save the rotate image")
+                .required(true)
+                .index(2))
+            .arg(Arg::with_name("amount")
+                .help("amount to rotate, 90, 180, 270 degrees")
+                .required(true)
+                .index(3)))
+        .subcommand(SubCommand::with_name("invert")
+            .about("Invert the image")
+            .arg(Arg::with_name("infile")
+                .help("input file to invert")
+                .required(true)
+                .index(1))
+            .arg(Arg::with_name("outfile")
+                .help("file to save the invert image")
+                .required(true)
+                .index(2)))
+        .subcommand(SubCommand::with_name("grayscale")
+            .about("Grayscale the image")
+            .arg(Arg::with_name("infile")
+                .help("input file to grayscale")
+                .required(true)
+                .index(1))
+            .arg(Arg::with_name("outfile")
+                .help("file to save the grayscale image")
+                .required(true)
+                .index(2)))
+        .subcommand(SubCommand::with_name("generate")
+            .about("Generate a solid color image")
+            .arg(Arg::with_name("outfile")
+                .help("file to save the generated image")
+                .required(true)
+                .index(1))
+            .arg(Arg::with_name("color")
+                .help("color to generate, e.g. 255 0 0")
+                .required(true)
+                .index(2)
+                .multiple(true)))
+        .subcommand(SubCommand::with_name("fractal")
+            .about("Generate a fractal image")
+            .arg(Arg::with_name("outfile")
+                .help("file to save the fractal image")
+                .required(true)
+                .index(1)))
+        .get_matches();
+
+    if let Some(matches) = app.subcommand_matches("blur") {
+        let infile = matches.value_of("infile").unwrap();
+        let outfile = matches.value_of("outfile").unwrap();
+        let amount: f32 = matches.value_of("amount").unwrap().parse().expect("Failed to parse a number");
+        blur(infile.to_string(), outfile.to_string(), amount);
+    } else if let Some(matches) = app.subcommand_matches("brighten") {
+        let infile = matches.value_of("infile").unwrap();
+        let outfile = matches.value_of("outfile").unwrap();
+        let amount: i32 = matches.value_of("amount").unwrap().parse().expect("Failed to parse a number");
+        brighten(infile.to_string(), outfile.to_string(), amount);
+    } else {
         print_usage_and_exit();
     }
-    let subcommand = args.remove(0);
-    match subcommand.as_str() {
-        // EXAMPLE FOR CONVERSION OPERATIONS
-        "blur" => {
-            if args.len() != 2 {
-                print_usage_and_exit();
-            }
-            let infile = args.remove(0);
-            let outfile = args.remove(0);
-            // **OPTION**
-            // Improve the blur implementation -- see the blur() function below
-            blur(infile, outfile);
-        }
-
-        // **OPTION**
-        // Brighten -- see the brighten() function below
-
-        // **OPTION**
-        // Crop -- see the crop() function below
-
-        // **OPTION**
-        // Rotate -- see the rotate() function below
-
-        // **OPTION**
-        // Invert -- see the invert() function below
-
-        // **OPTION**
-        // Grayscale -- see the grayscale() function below
-
-        // A VERY DIFFERENT EXAMPLE...a really fun one. :-)
-        "fractal" => {
-            if args.len() != 1 {
-                print_usage_and_exit();
-            }
-            let outfile = args.remove(0);
-            fractal(outfile);
-        }
-
-        // **OPTION**
-        // Generate -- see the generate() function below -- this should be sort of like "fractal()"!
-
-        // For everything else...
-        _ => {
-            print_usage_and_exit();
-        }
-    }
 }
+
 
 fn print_usage_and_exit() {
     println!("USAGE (when in doubt, use a .png extension on your filenames)");
@@ -94,84 +165,97 @@ fn print_usage_and_exit() {
     std::process::exit(-1);
 }
 
-fn blur(infile: String, outfile: String) {
+fn blur(infile: String, outfile: String, amount: f32) {
     // Here's how you open an existing image file
     let img = image::open(infile).expect("Failed to open INFILE.");
     // **OPTION**
-    // Parse the blur amount (an f32) from the command-line and pass it through
+    // Parse the blur amount (a f32) from the command-line and pass it through
     // to this function, instead of hard-coding it to 2.0.
-    let img2 = img.blur(2.0);
+    let img2 = img.blur(amount);
     // Here's how you save an image to a file.
     img2.save(outfile).expect("Failed writing OUTFILE.");
 }
 
-fn brighten(infile: String, outfile: String) {
+fn brighten(infile: String, outfile: String, amount: i32) {
     // See blur() for an example of how to open / save an image.
+    let img = image::open(infile).expect("Failed to open INFILE.");
 
     // .brighten() takes one argument, an i32.  Positive numbers brighten the
     // image. Negative numbers darken it.  It returns a new image.
+    let img2 = img.brighten(amount);
 
     // Challenge: parse the brightness amount from the command-line and pass it
     // through to this function.
+    img2.save(outfile).expect("Failed writing OUTFILE.");
 }
 
-fn crop(infile: String, outfile: String) {
+fn crop(infile: String, outfile: String, x : u32, y : u32, width : u32, height : u32) {
     // See blur() for an example of how to open an image.
+    let mut img = image::open(infile).expect("Failed to open INFILE.");
 
     // .crop() takes four arguments: x: u32, y: u32, width: u32, height: u32
     // You may hard-code them, if you like.  It returns a new image.
-
+    let img2 = img.crop(x, y, width, height);
     // Challenge: parse the four values from the command-line and pass them
     // through to this function.
 
     // See blur() for an example of how to save the image.
+    img2.save(outfile).expect("Failed writing OUTFILE.");
 }
 
-fn rotate(infile: String, outfile: String) {
+fn rotate(infile: String, outfile: String, amount: u32) {
     // See blur() for an example of how to open an image.
-
+    let img = image::open(infile).expect("Failed to open INFILE.");
     // There are 3 rotate functions to choose from (all clockwise):
     //   .rotate90()
     //   .rotate180()
     //   .rotate270()
     // All three methods return a new image.  Pick one and use it!
-
     // Challenge: parse the rotation amount from the command-line, pass it
     // through to this function to select which method to call.
+    let img2 = img.clone();
+    if amount == 90 {
+        img2.rotate90();
+    } else if amount == 180 {
+        img2.rotate180();
+    } else if amount == 270 {
+        img2.rotate270();
+    }
 
     // See blur() for an example of how to save the image.
+    img2.save(outfile).expect("Failed writing OUTFILE.");
 }
 
 fn invert(infile: String, outfile: String) {
     // See blur() for an example of how to open an image.
-
+    let img = image::open(infile).expect("Failed to open INFILE.");
     // .invert() takes no arguments and converts the image in-place, so you
     // will use the same image to save out to a different file.
+    let mut img2 = img.clone();
+    img2.invert();
 
     // See blur() for an example of how to save the image.
+    img2.save(outfile).expect("Failed writing OUTFILE.");
 }
 
 fn grayscale(infile: String, outfile: String) {
     // See blur() for an example of how to open an image.
-
+    let img = image::open(infile).expect("Failed to open INFILE.");
     // .grayscale() takes no arguments. It returns a new image.
-
+    let img2 = img.grayscale();
     // See blur() for an example of how to save the image.
+    img2.save(outfile).expect("Failed writing OUTFILE.");
 }
 
-fn generate(outfile: String) {
+fn generate(outfile: String, color: [u8; 3]) {
     // Create an ImageBuffer -- see fractal() for an example
-
+    let mut imgbuf = image::ImageBuffer::new(800, 800);
     // Iterate over the coordinates and pixels of the image -- see fractal() for an example
-
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
     // Set the image to some solid color. -- see fractal() for an example
-
-    // Challenge: parse some color data from the command-line, pass it through
-    // to this function to use for the solid color.
-
-    // Challenge 2: Generate something more interesting!
-
-    // See blur() for an example of how to save the image
+        *pixel = image::Rgb(color);
+    }
+    imgbuf.save(outfile).expect("Failed writing OUTFILE.");
 }
 
 // This code was adapted from https://github.com/PistonDevelopers/image
